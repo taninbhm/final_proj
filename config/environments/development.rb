e01fd2a055b6ad2b979b4c1fb657ac06a1e7a6af
tanin <t.banihashemi@gmailcom>
2019-03-18 09:37:49 -0500

    added and installed devise gem with extra fields

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+
+# for devise
+config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
@@ -44,8 +47,6 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
-# for devise
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
 
   # Debug mode disables concatenation and preprocessing of assets.
