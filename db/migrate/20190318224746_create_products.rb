bec4c6919c3f24164d64144017968c95dd0943d9
tanin <t.banihashemi@gmailcom>
2019-03-18 17:48:59 -0500

    Created Product Model

---
+++ db/migrate/20190318224746_create_products.rb
@@ -0,0 +1,14 @@
+class CreateProducts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :products do |t|
+      t.string :title
+      t.string :Image
+      t.text :description
+      t.decimal :price
+      t.string :stock
+      t.references :category, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
