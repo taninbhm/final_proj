a02e53560c174cb86c5419e33b263fb5d7c28a4d
tanin <t.banihashemi@gmailcom>
2019-03-24 21:17:57 -0500

    added LineItem scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_22_000209) do
+ActiveRecord::Schema.define(version: 2019_03_25_015747) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -36,6 +36,11 @@ ActiveRecord::Schema.define(version: 2019_03_22_000209) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "categories", force: :cascade do |t|
